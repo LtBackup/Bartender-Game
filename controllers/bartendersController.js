bf00e65550ace54281e12cb26a872d02c14d6633
LtBackup <jamespratt6@gmail.com>
2018-03-23 15:18:52 -0500

    starting on database logic

--- controllers/bartendersController.js
+++ controllers/bartendersController.js
@@ -3,32 +3,32 @@ const db = require("../models");
 //Defining methods for the bartendersController
 module.exports = {
   findAll: function(req, res) {
-    db.Book
+    db.Bartender
       .find(req.query)
       .sort({ date: -1 })
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   findById: function(req, res) {
-    db.Book
+    db.Bartender
       .findById(req.params.id)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   create: function(req, res) {
-    db.Book
+    db.Bartender
       .create(req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   update: function(req, res) {
-    db.Book
+    db.Bartender
       .findOneAndUpdate({ _id: req.params.id }, req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   remove: function(req, res) {
-    db.Book
+    db.Bartender
       .findById({ _id: req.params.id })
       .then(dbModel => dbModel.remove())
       .then(dbModel => res.json(dbModel))
