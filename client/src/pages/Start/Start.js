ab7b9fd89437b430db8bffa5c9475c0618327e51
LtBackup <jamespratt6@gmail.com>
2018-03-28 14:13:40 -0500

    adds login forms to front page

--- client/src/pages/Start/Start.js
+++ client/src/pages/Start/Start.js
@@ -4,6 +4,8 @@ import Jumbotron from "../../components/Jumbotron";
 //import { List, ListItem } from "../../components/List";
 import { Input, TextArea, FormBtn } from "../../components/Form";
 import { Col, Row, Grid } from "react-bootstrap";
+import Login from "../../components/Login";
+import SignUp from "../../components/SignUp";
 import CocktailAPI from "../../utils/CocktailAPI.js";
 import DBAPI from "../../utils/DBAPI.js";
 import './Start.css';
@@ -11,10 +13,31 @@ import './Start.css';
 
 class Start extends Component {
     state = {
-        loggedIn: false
+        existingUser: true,
+        loggedIn: false,
+        email: "",
+        password: ""
     };
 
     componentDidMount() {
+    }
+
+    toggleExisting = () => {
+        this.setState({ existingUser: !this.state.existingUser})
+    }
+  
+    validateForm() {
+      return this.state.email.length > 0 && this.state.password.length > 0;
+    }
+  
+    handleChange = event => {
+      this.setState({
+        [event.target.id]: event.target.value
+      });
+    }
+  
+    handleSubmit = event => {
+      event.preventDefault();
     }
 
     render() {
@@ -24,9 +47,9 @@ class Start extends Component {
                     <Jumbotron>
                         <h1>Let's Mix it Up!</h1>
                     </Jumbotron>
-                    <Row>
-                    //Login here
-                    </Row>
+                    {this.state.existingUser?
+                    <Login toggle={this.state} validateForm={this.validateForm} /> :
+                    <SignUp toggle={this.state} />}
                 </Row>
             </Grid>
         );
