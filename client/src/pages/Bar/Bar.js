85f1f177dd184753fa3d678c9757f79c30569198
LtBackup <jamespratt6@gmail.com>
2018-04-01 19:25:37 -0500

    resolves speed pouring bug

--- client/src/pages/Bar/Bar.js
+++ client/src/pages/Bar/Bar.js
@@ -23,23 +23,24 @@ class Bar extends Component {
     counters: [0, 0, 0, 0],
     drinkStatus: [], //0 = not filled, 1 = good fill, 2 = overfilled
     timer: 0,
+    animating: false,
     getColor: (status) => {
-      if(status === 0){
+      if (status === 0) {
         return "blue";
       }
-      else if( status === 1){
+      else if (status === 1) {
         return "green";
       }
-      else if (status === 2){
+      else if (status === 2) {
         return "red";
       }
     }
   };
 
   reset = () => {
-    this.setState({ counters: [0, 0, 0, 0], drinkStatus: []});
+    this.setState({ counters: [0, 0, 0, 0], drinkStatus: [] });
     this.getCocktail();
-    cancelAnimationFrame(this.count);
+    // cancelAnimationFrame(this.count);
   }
 
   toggleKeys = e => {
@@ -56,7 +57,7 @@ class Bar extends Component {
       else if (this.state.keysPressed.every(function (i) { return !i; })
         && this.state.drinkStatus.every(function (i) { return i === 1; })) {
         alert("Nice Pour! Let's mix another.");
-        
+
         DBAPI.updateMastery(this.state.username, this.state.currentDrinkData.strDrink);
         //increment user stats
         this.reset();
@@ -85,120 +86,123 @@ class Bar extends Component {
         }
       }
     })
-    
     requestAnimationFrame(this.count);
   }
 
-  getAvg = (strDash) => {
-    let values = strDash.split("-");
-    console.log("values", values);
-    return (parseFloat(values[0]) + parseFloat(values[1])) / 2;
-  }
+getAvg = (strDash) => {
+  let values = strDash.split("-");
+  console.log("values", values);
+  return (parseFloat(values[0]) + parseFloat(values[1])) / 2;
+}
 
-  toOunce = (strMeasure) => {
-    let parseArr = strMeasure.split(" "); //array of each piece
-    let result = 0;
-    parseArr.forEach((e, i) => {
-      console.log(e);
-      if ((e.toLowerCase() === "oz" || e.toLowerCase() === "cl" || e.toLowerCase() === "tbsp" || e.toLowerCase() === "tsp") && i !== parseArr.length - 1) {
-        parseArr.splice(i + 1, parseArr.length - (i + 1));
-      }
-    })
-    let measure = parseArr.pop();
-    for (let i = 0; i < parseArr.length; i++) {
-      if (parseArr[i].includes("-")) {
-        parseArr.splice(i, 1, this.getAvg(parseArr[i]));
-      }
-      result += eval(parseArr[i]);
+toOunce = (strMeasure) => {
+  let parseArr = strMeasure.split(" "); //array of each piece
+  let result = 0;
+  parseArr.forEach((e, i) => {
+    console.log(e);
+    if ((e.toLowerCase() === "oz" || e.toLowerCase() === "cl" || e.toLowerCase() === "tbsp" || e.toLowerCase() === "tsp") && i !== parseArr.length - 1) {
+      parseArr.splice(i + 1, parseArr.length - (i + 1));
     }
-    if (measure.toLowerCase() === "cl") {
-      return result * (3 / 10);
+  })
+  let measure = parseArr.pop();
+  for (let i = 0; i < parseArr.length; i++) {
+    if (parseArr[i].includes("-")) {
+      parseArr.splice(i, 1, this.getAvg(parseArr[i]));
     }
-    if (measure.toLowerCase() === "tbsp") {
-      return result / 2;
-    }
-    if (measure.toLowerCase() === "tsp") {
-      return result / 6;
-    }
-    return result;
+    result += eval(parseArr[i]);
   }
-
-  addListeners = () => {
-    document.removeEventListener("keydown", this.toggleKeys);
-    document.removeEventListener("keyup", this.toggleKeys);
-    document.addEventListener("keydown", this.toggleKeys);
-    document.addEventListener("keyup", this.toggleKeys);
+  if (measure.toLowerCase() === "cl") {
+    return result * (3 / 10);
   }
+  if (measure.toLowerCase() === "tbsp") {
+    return result / 2;
+  }
+  if (measure.toLowerCase() === "tsp") {
+    return result / 6;
+  }
+  return result;
+}
 
-  getCocktail = () => {
-    // CocktailAPI.getCocktail("Manhattan")
-    // .then(res => this.setState({currentDrinkData: res.data.drinks[0]}))
-    // .then(res => console.log(this.state.currentDrinkData))
-    // .catch(err => console.log(err));
-    // CocktailAPI.getRandom()
-    // .then(res => this.setState({currentDrinkData: res.data.drinks[0]}))
-    // .then(() => console.log(this.state.currentDrinkData))
-    // .catch(err => console.log(err));
-    CocktailAPI.getClassic()
-      .then(res => {
-        let validIngredients = [];
-        let validMeasurements = [];
-        let validComponents = [];
-        this.setState({ currentDrinkData: res.data.drinks[0] });
-        for (let k in res.data.drinks[0]) {
-          //console.log(k);
-          if (k.includes("Ingredient") && res.data.drinks[0][k]) {
-            validIngredients.push(res.data.drinks[0][k].trim());
-          }
-          if (k.includes("Measure") && res.data.drinks[0][k]) {
-            validMeasurements.push(res.data.drinks[0][k].trim());
-            console.log(res.data.drinks[0][k]);
-          }
+addListeners = () => {
+  document.removeEventListener("keydown", this.toggleKeys);
+  document.removeEventListener("keyup", this.toggleKeys);
+  document.addEventListener("keydown", this.toggleKeys);
+  document.addEventListener("keyup", this.toggleKeys);
+}
+
+getCocktail = () => {
+  // CocktailAPI.getCocktail("Manhattan")
+  // .then(res => this.setState({currentDrinkData: res.data.drinks[0]}))
+  // .then(res => console.log(this.state.currentDrinkData))
+  // .catch(err => console.log(err));
+  // CocktailAPI.getRandom()
+  // .then(res => this.setState({currentDrinkData: res.data.drinks[0]}))
+  // .then(() => console.log(this.state.currentDrinkData))
+  // .catch(err => console.log(err));
+  CocktailAPI.getClassic()
+    .then(res => {
+      let validIngredients = [];
+      let validMeasurements = [];
+      let validComponents = [];
+      this.setState({ currentDrinkData: res.data.drinks[0] });
+      for (let k in res.data.drinks[0]) {
+        //console.log(k);
+        if (k.includes("Ingredient") && res.data.drinks[0][k]) {
+          validIngredients.push(res.data.drinks[0][k].trim());
         }
-        validMeasurements.forEach((e, i) => {
-          let position = e.indexOf("oz")
-          if (e.toLowerCase().includes("oz") || e.toLowerCase().includes("cl") || e.toLowerCase().includes("tbsp")) {
-            const parsedMeasure = this.toOunce(e).toFixed(2);
-            validComponents.push({ ingredient: validIngredients[i], measurement: parsedMeasure });
-            this.state.drinkStatus.push(0);
-          }
-        })
-        this.setState({ ingredients: validComponents });
-      })
-      .then(res => {
+        if (k.includes("Measure") && res.data.drinks[0][k]) {
+          validMeasurements.push(res.data.drinks[0][k].trim());
+          console.log(res.data.drinks[0][k]);
+        }
+      }
+      validMeasurements.forEach((e, i) => {
+        let position = e.indexOf("oz")
+        if (e.toLowerCase().includes("oz") || e.toLowerCase().includes("cl") || e.toLowerCase().includes("tbsp")) {
+          const parsedMeasure = this.toOunce(e).toFixed(2);
+          validComponents.push({ ingredient: validIngredients[i], measurement: parsedMeasure });
+          this.state.drinkStatus.push(0);
+        }
+      });
+      this.setState({ ingredients: validComponents });
+    })
+    .then(res => {
+      if (!this.state.animating) {
+        console.log(this.state.animating);
         requestAnimationFrame(this.count);
-        console.log(this.state.currentDrinkData);
-        console.log(this.state.ingredients);
-      })
-      .catch(err => console.log(err));
-  }
+        this.setState({ animating: true });
+      }
+      console.log(this.state.currentDrinkData);
+      console.log(this.state.ingredients);
+    })
+    .catch(err => console.log(err));
+}
 
-  componentDidMount() {
-    this.getCocktail();
-    this.addListeners();
-    console.log(this.state.username);
-  }
+componentDidMount() {
+  this.getCocktail();
+  this.addListeners();
+  console.log(this.state.username);
+}
 
-  render() {
-    //console.log(this.state.ingredients);
-    return (
-      <Grid fluid>
-        <Row>
-          <div className="stage">
-            <DrinkCard
-              name={this.state.currentDrinkData.strDrink}
-              ingredients={this.state.ingredients}
-              counter={this.state.counters}
-              status={this.state.drinkStatus}
-              getColor={this.state.getColor} />
-            <Canvas />
-            <Serve />
-            <Rack />
-          </div>
-        </Row>
-      </Grid>
-    );
-  }
+render() {
+  //console.log(this.state.ingredients);
+  return (
+    <Grid fluid>
+      <Row>
+        <div className="stage">
+          <DrinkCard
+            name={this.state.currentDrinkData.strDrink}
+            ingredients={this.state.ingredients}
+            counter={this.state.counters}
+            status={this.state.drinkStatus}
+            getColor={this.state.getColor} />
+          <Canvas />
+          <Serve />
+          <Rack />
+        </div>
+      </Row>
+    </Grid>
+  );
+}
 }
 
 export default Bar;
