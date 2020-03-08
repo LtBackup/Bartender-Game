4f6ea3faecf113100f2d5247e88143c2378063ce
James Pratt <jamespratt6@gmail.com>
2020-03-08 18:57:55 -0500

    removes yarn and procfile

--- models/bartender.js
+++ models/bartender.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const bcrypt = require('bcrypt-nodejs'),
+const bcrypt = require('bcrypt'),
     SALT_WORK_FACTOR = 10;
 
 const bartenderSchema = new Schema({
